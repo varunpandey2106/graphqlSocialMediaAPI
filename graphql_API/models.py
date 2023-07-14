df3f8b612a933acce7a4d2c05c1612a6839daf92
varunpandey2106 <varun.pandey2106@gmail.com>
2023-07-14 09:22:35 +0530

    models, schema updated. migrations made

--- graphql_API/models.py
+++ graphql_API/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    name= models.CharField(max_length=250)
+    followers=models.ManyToManyField('graphql_API.User')
--- graphql_SM_API/schema.py
+++ graphql_SM_API/schema.py
@@ -1,6 +1,12 @@
 import graphene
+from graphene_django import DjangoObjectType
+from graphql_API import models
+
+class User(DjangoObjectType):
+    class Meta:
+        model=models.User
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    user = graphene.Field(User,id=graphene.Int())
 
 schema = graphene.Schema(query=Query)
